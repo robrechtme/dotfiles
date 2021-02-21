6412c0c05e4edb5f197fbace882d9d0f0b9506e0
robrechtme <robrechtmeersman@hotmail.com>
2021-02-21 11:42:57 +0100

    ✨ Add gitconfig & improve setup script

--- setup.sh
+++ setup.sh
@@ -4,22 +4,27 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")";
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-echo "📦 Installing packages using brew..."
-brew install bat
-brew install exa
-brew install romkatv/powerlevel10k/powerlevel10k
-brew install zsh-autosuggestions
+  echo "📦 Installing packages using brew..."
+  brew install bat
+  brew install exa
+  brew install romkatv/powerlevel10k/powerlevel10k
+  brew install zsh-autosuggestions
 elif [ "$OS_NAME" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
-echo "📦 Installing packages using apt..."
-apt install -y bat
-apt install -y exa
-apt install -y romkatv/powerlevel10k/powerlevel10k
-apt install -y zsh-autosuggestions
+  echo "📦 Installing packages using apt..."
+  apt install -y bat
+  apt install -y exa
+  apt install -y romkatv/powerlevel10k/powerlevel10k
+  apt install -y zsh-autosuggestions
 fi
 
 echo "🔗 Linking dotfiles..."
 
-ln -sf "$(pwd)/.zshrc" "$HOME/.zshrc" 
-ln -sf "$(pwd)/.p10k.zsh" "$HOME/.p10k.zsh" 
+FILES_TO_SYMLINK=".zshrc .p10k.zsh .gitconfig"
+for i in ${FILES_TO_SYMLINK[@]}; do
+  sourceFile="$(pwd)/$i"
+  targetFile="$HOME/$i"
+  ln -fs "$sourceFile" "$targetFile"
+  echo "$targetFile → $sourceFile"
+done
 
 echo "✅ Done!"
